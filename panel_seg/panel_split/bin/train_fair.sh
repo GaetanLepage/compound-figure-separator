e14b5c4e9a7a2adb824d95f4cfdb3759f56c546f
Gaetan Lepage <gaetan.lepage@grenoble-inp.org>
2020-05-06 08:15:31 +0200

    few modifications in training config

--- panel_seg/panel_split/bin/train_fair.sh
+++ panel_seg/panel_split/bin/train_fair.sh
@@ -1,4 +1,6 @@
 python panel_seg/panel_split/bin/train_net.py \
-    --num-gpus 2 \
-    --config-file panel_seg/panel_split/panel_split_image_clef_detectron.yaml
+    --num-gpus 1 \
+    --config-file panel_seg/panel_split/config_detectron.yaml \
+    --resume \
+    --eval-only
     #--dataset image_net
