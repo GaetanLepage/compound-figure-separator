b398f12a77e96bc3bda6519310a7b97161ee461d
Gaetan Lepage <gaetan.lepage@grenoble-inp.org>
2020-05-07 15:25:02 +0200

    managed to perform decent training. Changed some hyperparameters in config

--- panel_seg/panel_split/bin/train_fair.sh
+++ panel_seg/panel_split/bin/train_fair.sh
@@ -1,6 +1,5 @@
 python panel_seg/panel_split/bin/train_net.py \
-    --num-gpus 1 \
+    --num-gpus 2 \
     --config-file panel_seg/panel_split/config_detectron.yaml \
     --resume \
     --eval-only
-    #--dataset image_net
