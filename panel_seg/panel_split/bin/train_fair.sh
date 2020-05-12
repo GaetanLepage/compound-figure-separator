8e3cc48e5e5662454015f321bdb19f74cbaaac2b
Gaetan Lepage <gaetan.lepage@grenoble-inp.org>
2020-05-12 09:15:00 +0200

    few tries of changing training config

--- panel_seg/panel_split/bin/train_fair.sh
+++ panel_seg/panel_split/bin/train_fair.sh
@@ -2,4 +2,4 @@ python panel_seg/panel_split/bin/train_net.py \
     --num-gpus 2 \
     --config-file panel_seg/panel_split/config_detectron.yaml \
     --resume \
-    --eval-only
+    #--eval-only
--- panel_seg/panel_split/bin/train_net.py
+++ panel_seg/panel_split/bin/train_net.py
@@ -7,28 +7,25 @@ This scripts reads a given config file and runs the training or evaluation.
 It is an entry point that is made to train standard models in detectron2.
 
 In order to let one script support training of many models,
-this script contains logic that are specific to these built-in models and therefore
-may not be suitable for your own project.
-For example, your research project perhaps only needs a single "evaluator".
-
-Therefore, we recommend you to use detectron2 as an library and take
-this file as an example of how to use the library.
-You may want to write your own script with your datasets and other customizations.
 """
 
 import os
+import logging
+
+from collections import OrderedDict
 
 import detectron2.utils.comm as comm
 from detectron2.checkpoint import DetectionCheckpointer
-from detectron2.config import get_cfg
+from detectron2.config import CfgNode, get_cfg
 from detectron2.engine import DefaultTrainer, default_argument_parser, default_setup, launch
 from detectron2.evaluation import verify_results
 from detectron2.data.build import build_detection_test_loader
 from detectron2.data.dataset_mapper import DatasetMapper
 
-from panel_seg.panel_split.load_panel_split_datasets import register_image_clef_datasets
-from panel_seg.panel_split.loss_eval_hook import LossEvalHook
-from panel_seg.panel_split.panel_split_evaluator import PanelSplitEvaluator
+from panel_seg.panel_split.model.load_panel_split_datasets import register_image_clef_datasets
+from panel_seg.utils.loss_eval_hook import LossEvalHook
+from panel_seg.panel_split.model.panel_split_evaluator import PanelSplitEvaluator
+from panel_seg.panel_split.model.config import add_evaluation_config
 
 
 class Trainer(DefaultTrainer):
@@ -39,6 +36,25 @@ class Trainer(DefaultTrainer):
     "SimpleTrainer", or write your own training loop. You can use
     "tools/plain_train_net.py" as an example.
     """
+
+                 # evaluation_period: int,
+                 # evaluation_dataset_name: str):
+    def __init__(self,
+                 cfg: CfgNode):
+        """
+        TODO doc
+
+        Args:
+            cfg (CfgNode): TODO
+            evaluation_period (int): TODO
+            evaluation_dataset_name (str): TODO
+        """
+        self._validation_period = cfg.VALIDATION.VALIDATION_PERIOD
+        # self._validation_period = evaluation_period
+        self._validation_dataset_name = cfg.DATASETS.VALIDATION
+        # self._validation_dataset_name = evaluation_dataset_name
+        super().__init__(cfg)
+
 
     @classmethod
     def build_evaluator(cls, cfg, dataset_name, output_folder=None):
@@ -54,11 +70,31 @@ class Trainer(DefaultTrainer):
         return PanelSplitEvaluator(dataset_name, output_folder)
 
 
+    def build_hooks(self):
+        """
+        TODO
+        """
+        hooks = super().build_hooks()
+
+        hooks.insert(-1,
+                     LossEvalHook(eval_period=self._validation_period,
+                                  model=self.model,
+                                  data_loader=build_detection_test_loader(
+                                      cfg=self.cfg,
+                                      dataset_name=self._validation_dataset_name,
+                                      mapper=DatasetMapper(cfg=self.cfg,
+                                                           is_train=True))))
+        return hooks
+
+
+
+
 def setup(args):
     """
     Create configs and perform basic setups.
     """
     cfg = get_cfg()
+    add_evaluation_config(cfg)
     cfg.merge_from_file(args.config_file)
     cfg.merge_from_list(args.opts)
     cfg.freeze()
@@ -70,6 +106,7 @@ def main(args):
     cfg = setup(args)
 
     register_image_clef_datasets()
+
     if args.eval_only:
         model = Trainer.build_model(cfg)
         DetectionCheckpointer(model,
@@ -85,27 +122,25 @@ def main(args):
     # If you'd like to do anything fancier than the standard training logic,
     # consider writing your own training loop or subclassing the trainer.
     trainer = Trainer(cfg)
+                      # args.evaluation_period,
+                      # args.evaluation_dataset_name)
     trainer.resume_or_load(resume=args.resume)
-    # trainer.register_hooks(
-        # [LossEvalHook(eval_period=cfg.VALIDATION.VALIDATION_PERIOD,
-                      # model=model,
-                      # data_loader=build_detection_test_loader(
-                          # cfg=cfg,
-                          # dataset_name=cfg.DATASETS.VALIDATION,
-                          # mapper=DatasetMapper(cfg=cfg,
-                                               # is_train=True)))])
     return trainer.train()
 
 
 if __name__ == "__main__":
-    args = default_argument_parser().parse_args()
+    parser = default_argument_parser()
+    parser.add_argument("--eval-period",
+                        help="The number of iterations between two validations.",
+                        type=int,
+                        default=5000)
+
+    args = parser.parse_args()
     print("Command Line Args:", args)
     # load_datasets(args.dataset)
-    launch(
-        main,
-        args.num_gpus,
-        num_machines=args.num_machines,
-        machine_rank=args.machine_rank,
-        dist_url=args.dist_url,
-        args=(args,),
-    )
+    launch(main,
+           args.num_gpus,
+           num_machines=args.num_machines,
+           machine_rank=args.machine_rank,
+           dist_url=args.dist_url,
+           args=(args,))
