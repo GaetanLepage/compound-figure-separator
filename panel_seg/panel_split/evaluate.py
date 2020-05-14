52806039409124b76c0a180b11fd33a8feb8858e
Gaetan Lepage <gaetan.lepage@grenoble-inp.org>
2020-05-14 18:06:05 +0200

    correcting evaluation for panel splitting

--- panel_seg/panel_split/evaluate.py
+++ panel_seg/panel_split/evaluate.py
@@ -2,6 +2,8 @@
 Module to evaluate the panel splitting task metrics.
 """
 
+from typing import List
+
 
 def evaluate_predictions(figure_generator: str):
     """
@@ -22,7 +24,6 @@ def evaluate_predictions(figure_generator: str):
     # ImageCLEF
     sum_imageclef_accuracies = 0.0
 
-
     # Stats to compute mAP
     overall_correct_count = 0
     sum_recalls = 0.0
@@ -37,8 +38,7 @@ def evaluate_predictions(figure_generator: str):
         overall_gt_count += len(figure.gt_panels)
         overall_pred_count += len(figure.pred_panels)
 
-
-        # ImageCLEF
+        # 1) ImageCLEF accuracy
         num_correct_imageclef = figure.get_num_correct_predictions(
             use_overlap_instead_of_iou=True)
         k = max(len(figure.gt_panels), len(figure.pred_panels))
@@ -46,42 +46,38 @@ def evaluate_predictions(figure_generator: str):
 
         sum_imageclef_accuracies += imageclef_accuracy
 
-
-        # Usual mAP
+        # 2) Usual metrics
         num_correct_iou_thresh = figure.get_num_correct_predictions(
             use_overlap_instead_of_iou=False)
         overall_correct_count += num_correct_iou_thresh
 
         # recall = TP / TP + FN
-        recall = num_correct_imageclef / len(figure.gt_panels)
+        recall = num_correct_iou_thresh / len(figure.gt_panels)
         sum_recalls += recall
 
         # precision = TP / TP + FP
         if len(figure.pred_panels) == 0:
             precision = 0
         else:
-            precision = num_correct_imageclef / len(figure.pred_panels)
+            precision = num_correct_iou_thresh / len(figure.pred_panels)
         sum_precisions += precision
 
 
     imageclef_accuracy = sum_imageclef_accuracies / num_samples
 
-    overall_recall = overall_correct_count / overall_gt_count
-    overall_precision = overall_correct_count / overall_pred_count
+    recall = overall_correct_count / overall_gt_count
+    precision = overall_correct_count / overall_pred_count
     # mAP = TP / TP + FP
     mAP = overall_correct_count / overall_pred_count
 
-    print("ImageCLEF Accuracy (overlap threshold = 0.66): {}\n"\
-            "Precision: {}\n"\
-            "Recall: {}\n"\
-            "mAP (IoU threshold = 0.5): {}".format(imageclef_accuracy,
-                                                   overall_precision,
-                                                   overall_recall,
-                                                   mAP))
+    print(f"ImageCLEF Accuracy (overlap threshold = 0.66): {imageclef_accuracy:.3f}\n"\
+            f"Precision: {precision:.3f}\n"\
+            f"mAP (IoU threshold = 0.5): {mAP:.3f}")
+
     metrics = {
         'image_clef_accuracy': imageclef_accuracy,
-        'precision': overall_precision,
-        'recall': overall_recall,
+        'precision': precision,
+        'recall': recall,
         'mAP': mAP
         }
 
