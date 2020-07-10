646297692a9fa736d7a0ebd5cf9857ce6f8d5bb6
Gaetan Lepage <gaetan.lepage@grenoble-inp.org>
2020-07-10 08:55:55 +0200

    Init for main task submodule

---
+++ compfigsep/compound_figure_separation/__init__.py
@@ -0,0 +1,32 @@
+"""
+#############################
+#        CompFigSep         #
+# Compound Figure Separator #
+#############################
+
+GitHub:         https://github.com/GaetanLepage/compound-figure-separator
+
+Author:         Gaétan Lepage
+Email:          gaetan.lepage@grenoble-inp.org
+Date:           Spring 2020
+
+Master's project @HES-SO (Sierre, SW)
+
+Supervisors:    Henning Müller (henning.mueller@hevs.ch)
+                Manfredo Atzori (manfredo.atzori@hevs.ch)
+
+Collaborator:   Niccolò Marini (niccolo.marini@hevs.ch)
+
+
+#######################################################################
+Compound figure separation module.
+Achieve both panel segmentation, caption splitting and results merging.
+
+Input: Compound (i.e. multi-panel) figure/image
+       + its caption text.
+
+Output: A list of sub-figures including :
+    * Panel location
+    * Label location
+    * Sub-caption text
+"""
