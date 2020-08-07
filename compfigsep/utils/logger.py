9aa0dd500a8c70b35c949ed626f84a2029f4d235
Gaetan Lepage <gaetan.lepage@grenoble-inp.org>
2020-08-07 18:17:49 +0200

    blank file for future logging management

---
+++ compfigsep/utils/logger.py
@@ -0,0 +1,25 @@
+"""
+#############################
+#        CompFigSep         #
+# Compound Figure Separator #
+#############################
+
+GitHub:         https://github.com/GaetanLepage/compound-figure-separator
+
+Author:         Gaétan Lepage
+Email:          gaetan.lepage@grenoble-inp.org
+Date:           Spring 2020
+
+Master's project @HES-SO (Sierre, SW)
+
+Supervisors:    Henning Müller (henning.mueller@hevs.ch)
+                Manfredo Atzori (manfredo.atzori@hevs.ch)
+
+Collaborators:  Niccolò Marini (niccolo.marini@hevs.ch)
+                Stefano Marchesin (stefano.marchesin@unipd.it)
+
+
+########################################
+CompFigSep logger configuration routine.
+"""
+
