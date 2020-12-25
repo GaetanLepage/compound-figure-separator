95cfe81824895314f1cb23fa97a3ca9f3dc6ebc7
Gaetan Lepage <gaetan.lepage@inria.fr>
2020-12-25 23:49:33 +0100

    compound figure separation main program

---
+++ compfigsep/compound_figure_separation/compound_figure_separation.py
@@ -0,0 +1,32 @@
+"""
+#############################
+#        CompFigSep         #
+# Compound Figure Separator #
+#############################
+
+GitHub:         https://github.com/GaetanLepage/compound-figure-separator
+
+Author:         Gaétan Lepage
+Email:          gaetan.lepage@grenoble-inp.org
+Date:           Spring 2020
+
+Master's project @HES-SO (Sierre, SW)
+
+Supervisors:    Henning Müller (henning.mueller@hevs.ch)
+                Manfredo Atzori (manfredo.atzori@hevs.ch)
+
+Collaborators:  Niccolò Marini (niccolo.marini@hevs.ch)
+                Stefano Marchesin (stefano.marchesin@unipd.it)
+
+
+###################################################################
+Main function running the full compound figure separation pipeline.
+"""
+
+from ..data.figure_generators import FigureGenerator
+
+
+def compound_figure_separation(figure_generator: FigureGenerator):
+    """
+    TODO
+    """
