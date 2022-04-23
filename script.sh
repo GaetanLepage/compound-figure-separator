39ceca4b20fcf1dd4b22ce3c8fab7577e6ff7b1f
Gaetan Lepage <gaetan.lepage@inria.fr>
2022-04-24 00:42:33 +0200

    remi

--- script.sh
+++ script.sh
@@ -1,3 +1,10 @@
-SCRIPT=
+#!/bin/sh
 
-python compfigsep/$SCRIPT
+export PYTHONPATH=.
+export NUM_GPUS=1
+
+SCRIPT=panel_splitting/bin/train_panel_seg.sh
+
+bash compfigsep/$SCRIPT
+
+# python compfigsep/$SCRIPT
