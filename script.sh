6456c631800f7e366f037155d3b1e57bfb87f0f8
Gaetan Lepage <gaetan.lepage@inria.fr>
2022-05-12 22:33:35 +0200

    container + dependencies

--- script.sh
+++ script.sh
@@ -3,8 +3,27 @@
 export PYTHONPATH=.
 export NUM_GPUS=1
 
-SCRIPT=panel_splitting/bin/train_panel_seg.sh
 
-bash compfigsep/$SCRIPT
+# TASK="panel_splitting"
+TASK="label_recognition"
+# TASK="panel_segmentation"
 
-# python compfigsep/$SCRIPT
+# SCRIPT_NAME="test_imageclef.sh"
+# SCRIPT_NAME="train_imageclef.sh"
+# SCRIPT_NAME="train_panel_seg.sh"
+SCRIPT_NAME="train.sh"
+
+SCRIPT_PATH=$TASK/bin/$SCRIPT_NAME
+# SCRIPT="panel_splitting/bin/train_panel_seg.sh"
+# SCRIPT="panel_splitting/bin/test_imageclef.sh"
+
+
+COMMAND="bash compfigsep/${SCRIPT_PATH}"
+# COMMAND="which python"
+
+if [[ `hostname` =~ "bigfoot" ]]; then
+    source /applis/environments/conda.sh
+    conda activate compfigsep
+fi
+# \time -f "Command took %E" $COMMAND $@
+$COMMAND $@
